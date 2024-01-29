871a0aa3925460dc581e9bd445fbf9bad6fb7460
Marc Lelarge <marc.lelarge@gmail.com>
2024-01-29 10:17:37 +0100

    random

--- randomw.py
+++ randomw.py
@@ -45,7 +45,7 @@ name_file = '/scratch/lelarge/experiments-gnn-match/randomw_ER_2res256_500_4.npy
 
 config_model, device = get_device_config(path_config)
 
-config_model['data']['test'] = {'num_examples_test': 20,
+new_test = {'num_examples_test': 20,
   'n_vertices': 500,
   'generative_model': 'ErdosRenyi',
   'noise_model': 'ErdosRenyi',
@@ -60,6 +60,8 @@ model_r = model_r.half()
 model_r = model_r.to(device)
 data_pb_dir = DATA_PB_DIR
 t_pipeline_ER = Pipeline(path_config,data_pb_dir)
+t_pipeline_ER.data['test'] = new_test
+
 all_acc_gnn, all_qap_gnn, all_p_gnn, all_qap_gnnc,  all_acc_gnn_c = make_all_rgnn(list_noises,t_pipeline_ER, model_r,device)
 
 with open(name_file, 'wb') as f:
